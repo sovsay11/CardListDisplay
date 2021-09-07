5606cd40c30444890219fca86fc04a8d8eacb524
Noah <sovannarasay11@gmail.com>
2021-09-06 23:21:29 -0600

    Implements listview and adds minor style for the control

--- CardListDisplay/MainPage.xaml.cs
+++ CardListDisplay/MainPage.xaml.cs
@@ -14,15 +14,11 @@ namespace CardListDisplay
     public partial class MainPage : ContentPage
     {
         // declare collections for account information
-        private List<string> accounts;
-        private List<string> showAccounts;
         private List<Account> fullAccounts;
         public MainPage()
         {
             InitializeComponent();
             // initialize collections with default values
-            accounts = new List<string>() { "Apple", "Banana", "Cherry", "Donut", "Eclair", "Fruit" }; // initialize the list
-            showAccounts = new List<string>() { "Bunnies", "Kittens", "Puppies", "Cubs", "Babies", "Random" };
             fullAccounts = new List<Account>();
             // Retrieve info from account text file
             LoadAccountDetails(SearchEntry.Text);
@@ -67,59 +63,7 @@ namespace CardListDisplay
         /// </summary>
         private void LoadCards()
         {
-            // reset the layout
-            MainLayout.Children.Clear();
-
-            // adding cards to the list!
-            foreach (var item in fullAccounts)
-            {
-                // Define overall layout and UI elements
-                StackLayout innerLayout = new StackLayout(); // new stack layout, this will hold the text content
-                // labels with styles and text
-                Label accountLabel = new Label { Text = item.AccountName, Style = (Style)Application.Current.Resources["accountLabelStyle"] };
-                Label showLabel = new Label { Text = "Tap to View" };
-                Label username = new Label { Text = $"Username:\t\t{item.Username}" };
-                Label password = new Label { Text = $"Password:\t\t\t{item.Password}" };
-
-                // Logic for tap gestures when frame/card is pressed
-                bool toggle = true;
-                TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer(); // new tap gesture recognizer
-                                                                                        // add a tap event
-                tapGestureRecognizer.Tapped += (object sender, EventArgs e) =>
-                {
-                    Frame tFrame = (Frame)sender; // grab the object details
-                    if (toggle)
-                    {
-                        tFrame.SetDynamicResource(StyleProperty, "expandAccountCardStyle");
-                        showLabel.IsVisible = false;
-                        //showLabel.Text = "Tap to Hide";
-                        //tFrame.Resources["accountCardStyle"] = Application.Current.Resources["expandAccountCardStyle"]; // change the resources style
-                        toggle = false;
-                    }
-                    else
-                    {
-                        tFrame.SetDynamicResource(StyleProperty, "accountCardStyle");
-                        showLabel.IsVisible = true;
-                        //showLabel.Text = "Tap to View";
-                        //tFrame.Resources["expandAccountCardStyle"] = Application.Current.Resources["accountCardStyle"]; // change the resources style
-                        toggle = true;
-                    }
-                    //Resources["accountCardStyle"] = Application.Current.Resources["expandAccountCardStyle"];
-                };
-
-                // new frame, this is the main background of the template where all the elements will be placed
-                Frame frame = new Frame();
-                frame.SetDynamicResource(StyleProperty, "accountCardStyle"); // setting the style of the frame, needs to be dynamic
-                frame.GestureRecognizers.Add(tapGestureRecognizer); // add a tap event for the frame
-
-                // adding everything to the frame and layouts
-                innerLayout.Children.Add(accountLabel);
-                innerLayout.Children.Add(showLabel);
-                innerLayout.Children.Add(username);
-                innerLayout.Children.Add(password);
-                frame.Content = innerLayout;
-                MainLayout.Children.Add(frame);
-            }
+            LstViewAccounts.ItemsSource = fullAccounts;
         }
 
         private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
